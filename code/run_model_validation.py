Fix kl eval error for sum(virtual_belief) =0

code/run_model_validation.py
@@ -268,6 +268,9 @@ if __name__ == "__main__":
     AGENT_GEN_SPACE = ["C1", "C2", "C3", "A1", "A2", "A3"]
     TAU_GEN_SPACE = np.linspace(0.01, 0.5, 2).tolist()
     LAMBDA_GEN_SPACE = np.linspace(0, 1, 2).tolist()
+    AGENT_GEN_SPACE = ["A3"]
+    TAU_GEN_SPACE = [0.01]
+    LAMBDA_GEN_SPACE = [0.582, 0.748]
 
     # Define parameter estimation candidate space
     AGENT_CAND_SPACE = ["C1", "C2", "C3", "A1", "A2", "A3"]
@@ -275,7 +278,7 @@ if __name__ == "__main__":
     LAMBDA_CAND_SPACE = np.linspace(0.25, 0.75, 2).tolist()
 
     # Configure quick test
-    IS_QUICK_TEST = True
+    IS_QUICK_TEST = False
     TEST_N_BLOCKS = 1
     TEST_N_ROUNDS = 10
     TEST_N_TRIALS = 12

code/utilities/agent.py
@@ -434,7 +434,7 @@ class Agent:
             post_belief_state = (
                 prior_belief_state
                 * self.bayes_comps.lklh[action, s_1, s2_giv_s1, obs, :, :])
-            print('zero_sum in bayesian likelihood occurred')
+            print('sum of prio * lklh = 0, leaving out normalization')
             # debug = 'here'
         else:
             post_belief_state = (
@@ -637,18 +637,24 @@ class Agent:
         # end = time.time()
         # print(f'After masking zeros: {end - start}')
 
+        # Ensure KL is zero, if virtual postior and current belief state are 
+        # equal
         if np.all(np.around(p_x, 10) == np.around(q_x, 10)):
-            kl_mask = 0.
+            kl = 0.
+
+        # or sum over all dimensions of virtual belief for potential obs after
+        # given action are zero, i.e. the agent is knows with certainty that
+        # potential observation is inprobable (impossible), e.g. treasure on
+        # grey field
+        elif np.sum(np.sum(p_x)) == 0:
+            kl = 0.
 
         else:
-            # start = time.time()
-            # diffs = np.where(p_x != q_x)
             with np.errstate(divide="ignore", invalid="ignore"):
-                kl_mask = np.sum(p_x * np.ma.masked_invalid(np.log(p_x / q_x)))
-            # end = time.time()
-            # print(f'Using ma.masked all_in_one: {end - start}')
+                kl = np.sum(p_x * np.ma.masked_invalid(np.log(p_x / q_x)))
+                # TODO: masked value troublesome
 
-        return kl_mask
+        return kl
 
     def eval_kl(self):
         """Evaluate KL divergence between the agent's belief state on trial t
@@ -681,8 +687,13 @@ class Agent:
                     self.task.s2_t[new_s1], obs)
 
                 # Evaluate KL divergence
+                kl_value = self.evaluate_kl_divergence(
+                    self.virt_b[i][obs], self.p_s_giv_o)
+                
                 self.kl_giv_a_o[i, obs] = self.evaluate_kl_divergence(
                     self.virt_b[i][obs], self.p_s_giv_o)
+
+                stop = "here"
 
     def evaluate_action_valences(self):
         """Evaluate action valences"""

code/utilities/modelling.py
@@ -14,7 +14,7 @@ class BehavioralModel:
     a_t : array_like
         Action value in trial t
     """
-    p_a_giv_h: np.ndarray  # likelihood fucntion of action giv history and tau
+    p_a_giv_h: np.ndarray  # likelihood function of action giv history and tau
     rvs: np.ndarray
     log_likelihood: float = np.nan
     action_t = np.nan  # agent action
@@ -32,8 +32,7 @@ class BehavioralModel:
             np.exp((1 / self.tau) * self.agent.valence_t))
 
     def eval_rvs(self):
-        """Evaluate action according to sample from multinomial distribution
-        TODO what does rvs stand for"""
+        """Evaluate action according to sample from multinomial distribution"""
         rng = np.random.default_rng()
         self.rvs = rng.multinomial(1, self.p_a_giv_h)
 

code/utilities/simulation_methods.py
@@ -400,7 +400,7 @@ class Simulator():
 
         n_blocks = data.block.max()
         n_rounds = data.round_.max()
-        n_trials = data.trial.max() - 1  # subtract 1 bc data trials = 13
+        n_trials = data.trial.max() - 1  # subtract 1 bc data n trials = 13
 
         llhs_all_blocks = np.full((n_blocks, 1), np.nan)
 